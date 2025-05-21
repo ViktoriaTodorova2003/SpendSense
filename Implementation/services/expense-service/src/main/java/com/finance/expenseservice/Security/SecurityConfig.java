d6983c768363d3c414c8e2dc111bd53c083dca87
Victoria Todorova <victoria.todorova.2003@gmail.com>
2025-05-21 11:58:29 +0200

    initial commit, but mid project

---
+++ Implementation/services/expense-service/src/main/java/com/finance/expenseservice/Security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.finance.expenseservice.Security; // Adjust your package name accordingly
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final AuthHeaderFilter authHeaderFilter;
+
+    public SecurityConfig(AuthHeaderFilter authHeaderFilter) {
+        this.authHeaderFilter = authHeaderFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll()  // Allow all other requests
+                );
+//                .addFilterBefore(authHeaderFilter, UsernamePasswordAuthenticationFilter.class);  // Add the custom filter
+
+        return http.build();
+    }
+}
+
+
