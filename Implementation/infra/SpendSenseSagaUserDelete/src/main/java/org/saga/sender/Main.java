daaf78807b93e27937857ef1a17357b0d5416abc
Victoria Todorova <victoria.todorova.2003@gmail.com>
2025-06-04 11:36:37 +0200

    Updates BudgetController to fix / - no/ issue

--- Implementation/infra/SpendSenseSagaUserDelete/src/main/java/org/saga/sender/Main.java
+++ Implementation/infra/SpendSenseSagaUserDelete/src/main/java/org/saga/sender/Main.java
@@ -3,6 +3,6 @@ package org.saga.sender;
 public class Main {
         public static void main(String[] args) {
             MessageSender sender = new MessageSender();
-            sender.sendMessage(123);
+            sender.sendMessage(1);
     }
 }
--- Implementation/services/budget-service/src/main/java/com/finance/budget_service/BudgetController.java
+++ Implementation/services/budget-service/src/main/java/com/finance/budget_service/BudgetController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/budgets")
+@RequestMapping({"/api/budgets", "/api/budgets/"})
 public class BudgetController {
 
     @Autowired
