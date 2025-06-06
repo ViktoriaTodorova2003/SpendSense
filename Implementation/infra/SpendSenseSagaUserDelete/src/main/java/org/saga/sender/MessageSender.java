746717f957c3563d6bec1df653e7370d9bbcfa98
Victoria Todorova <victoria.todorova.2003@gmail.com>
2025-06-06 08:59:20 +0200

    Idk

--- Implementation/infra/SpendSenseSagaUserDelete/src/main/java/org/saga/sender/MessageSender.java
+++ Implementation/infra/SpendSenseSagaUserDelete/src/main/java/org/saga/sender/MessageSender.java
@@ -9,6 +9,7 @@ import com.google.gson.Gson;
 public class MessageSender
 {
     private static final String CONNECTION_STRING = "Endpoint=sb://spendsense-service-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=LzYR9PAa13NXl3F7EPq97P5HUHzRO9LlM+ASbDSbm0k=";
+
     private static final String QUEUE_NAME = "spendsense-queue";
 
     public void sendMessage(int userId) {
